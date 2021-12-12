a22f220050be9be27cc994e9795360f2fdc572b6
MuntaqimRahman <rmuntaqim@yahoo.com>
2021-12-12 01:25:56 -0500

    Removed resume page

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,84 +1,11 @@
-import { useState, useEffect } from "react";
 import styled from "styled-components";
 
 import LandingComponent from "../../components/landing-component/landing-component";
-import ResumeTable from "../../components/resume-table/resume-table";
 
 import { introText} from "../../static/static-text";
-import {dataTypes, resumeDataState} from "../../static/resume-data-information";
 
-import {
-  GetExperiences,
-  GetResumeProjects,
-  GetActivities,
-  GetSkills,
-} from "../../api/resume_api";
-
-
-const initialData = [
-  {
-    type: dataTypes.experiences,
-    values: [],
-  },
-  {
-    type: dataTypes.projects,
-    values: [],
-  },
-  {
-    type: dataTypes.activities,
-    values: [],
-  },
-  {
-    type: dataTypes.skills,
-    values: [],
-  },
-];
 
 const AboutPage = () => {
-  const [isGettingData, setIsGettingData] = useState<boolean>(true);
-  const [isDataLoaded, setIsDataLoaded] = useState<boolean>(false);
-  const [resumeData, setResumeData] = useState<resumeDataState[]>(initialData)
-
-
-  useEffect(() => {
-    getData();
-  },[])
-
-  const getData = async () => {
-    try {
-      const experiences = await GetExperiences();
-      const resumeProjects = await GetResumeProjects();
-      const activities = await GetActivities();
-      const skills = await GetSkills();
-
-      const newResumeState = [
-        {
-          type: dataTypes.experiences,
-          values: experiences,
-        },
-        {
-          type: dataTypes.projects,
-          values: resumeProjects,
-        },
-        {
-          type: dataTypes.activities,
-          values: activities,
-        },
-        {
-          type: dataTypes.skills,
-          values: skills,
-        },
-      ];
-
-      setResumeData(newResumeState);
-      setIsGettingData(false);
-      setIsDataLoaded(true);
-    } catch (err) {
-      console.log(err);
-      setIsGettingData(false);
-      setIsDataLoaded(false);
-    }
-  };
 
   const AboutPageContainer = styled.div`
     display: flex;
@@ -127,9 +54,6 @@ const AboutPage = () => {
         <IntroText >Welcome To My Website</IntroText>
         <MainText>{introText} </MainText>
       </DescriptionContainer>
-
-      <IntroText >Resume</IntroText>
-      <ResumeTable isGettingData={isGettingData} isDataLoaded={isDataLoaded} resumeData={resumeData}/>
     </AboutPageContainer>  
   );
 };
